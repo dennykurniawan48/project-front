cd800a4b4a812d07606bac7dade423b14a108108
dennykurniawan48 <kurniawan.denny.48.com>
2021-10-14 14:42:24 +0700

    initial main page

--- components/HeaderNotLogin.js
+++ components/HeaderNotLogin.js
@@ -1,7 +1,7 @@
 import Link from "next/link"
 
 const HeaderNotLogin = () => {
-    return <header className="absolute bg-white w-full">
+    return <header className="relative bg-white w-full">
         <div className="flex justify-between m-4 items-center">
             <h1 className="text-xl font-bold">Toko Mafia</h1>
             <button className="bg-blue-500 py-2 px-4 text-white"><Link href="/login">Login</Link></button>
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,56 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { authAction } from '../store/auth'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import axios from 'axios'
+import HeaderNotLogin from '../components/HeaderNotLogin'
+import Constant from '../components/Constant'
 
-export default function Home() {
+export default function Home(props) {
   const dispatch = useDispatch()
   const loggedIn = useSelector(state => state.auth.isLoggedIn);
   const router = useRouter()
 
-  useEffect(() => {
-    console.log("Call token" + loggedIn)
-    const token = localStorage.getItem('_token')
-    if(!token){
-      dispatch(authAction.logout())
-      router.replace('/login')
-    }
-  }, [loggedIn])
+  console.log(props.data)
+
+  const data = props.data.map(item => <div className="ml-3 mt-3"><img src={Constant.BaseUrl + item.main_image}/><h1>{item.product_name}</h1></div>)
+
+  // useEffect(() => {
+  //   console.log("Call token" + loggedIn)
+  //   const token = localStorage.getItem('_token')
+  //   if(!token){
+  //     dispatch(authAction.logout())
+  //     router.replace('/login')
+  //   }
+  // }, [loggedIn])
+
   const logoutHandler = () => {
     localStorage.removeItem('_token')
     dispatch(authAction.logout())
   }
 
-  if(loggedIn){
-  return <>
+  return <div>
+  {!loggedIn && <HeaderNotLogin/>}
+  <div className="m-4 grid grid-cols-3" >
+    {data}
+  </div>
     <button onClick={logoutHandler}>Logout</button>
-  </>
-  }else{
-    return <></>
+  </div>
+
+}
+
+export const getStaticProps = async() => {
+  let data = []
+  try{
+    const response = await axios.get('/products')
+    data = response.data.data
+    console.log(data)
+  }catch(e){
+    console.log(e.response.data)
+  }finally{
+    return {
+      props:{
+        data
+      }
+    }
   }
 }
